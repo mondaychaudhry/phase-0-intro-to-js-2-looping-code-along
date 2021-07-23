52693dc80d306ec450e9d8ada4a28250d6c4cad1
Monday <sobaata@gmail.com>
2021-07-22 22:16:35 -0400

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+
+let x =[ 'Lisa', 'Kaitlin', 'Jan' ]
+let y = 'surprise'
+
+function writeCards(x,y){
+ let z = []
+  for (let i = 0; i < x.length; i++){
+    z.push(`Thank you, ${x[i]}, for the wonderful ${y} gift!`)
+    console.log(i)
+  }
+  return z
+} 
+
+function countDown(f) {
+  while (f > 0){
+    console.log(f) 
+    f--
+       }
+  return console.log(f)
+  }
